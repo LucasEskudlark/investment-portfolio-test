5a600a4bc450c2c8c9fa35168f980203976c9846
Lucas Eskudlark <lucaseskudlark@gmail.com>
2021-12-20 19:30:39 -0300

    Setting "Asset" class

--- src/com/wallet/Asset.java
+++ src/com/wallet/Asset.java
@@ -1,4 +1,45 @@
 package com.wallet;
 
 public class Asset {
+    private String assetCode;
+    private Integer amount;
+    private Double pricePerUnity;
+
+    // Constructor
+    public Asset(String assetCode, Integer amount, Double pricePerUnity){
+        this.assetCode = assetCode;
+        this.amount = amount;
+        this.pricePerUnity = pricePerUnity;
+    }
+
+
+    public String getAssetCode() {
+        return assetCode;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Double getPricePerUnity() {
+        return pricePerUnity;
+    }
+
+    public void setPricePerUnity(Double pricePerUnity) {
+        this.pricePerUnity = pricePerUnity;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Asset{" +
+                "assetCode='" + assetCode + '\'' +
+                ", amount=" + amount +
+                ", pricePerUnity=" + pricePerUnity +
+                '}';
+    }
 }
